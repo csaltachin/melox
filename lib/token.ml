f4183f4ae7c6f59702fffbb80d91d49c6be717a3
Carlos Solano <csaltachin@gmail.com>
2024-03-08 16:17:44 -0600

    Pretty-print tokens

--- lib/token.ml
+++ lib/token.ml
@@ -52,5 +52,51 @@ type t =
   ; start_col : int
   }
 
+let string_of_raw raw_token =
+  match raw_token with
+  | LeftParen -> "LeftParen"
+  | RightParen -> "RightParen"
+  | LeftBrace -> "LeftBrace"
+  | RightBrace -> "RightBrace"
+  | Comma -> "Comma"
+  | Dot -> "Dot"
+  | Plus -> "Plus"
+  | Minus -> "Minus"
+  | Slash -> "Slash"
+  | Star -> "Star"
+  | Semicolon -> "Semicolon"
+  | Bang -> "Bang"
+  | BangEqual -> "BangEqual"
+  | Equal -> "Equal"
+  | EqualEqual -> "EqualEqual"
+  | Greater -> "Greater"
+  | GreaterEqual -> "GreaterEqual"
+  | Less -> "Less"
+  | LessEqual -> "LessEqual"
+  | Identifier ident -> "Identifier |> " ^ ident
+  | String literal -> "StringLiteral |> \"" ^ literal ^ "\""
+  | Number number -> "NumberLiteral |> " ^ string_of_float number
+  | And -> "And"
+  | Class -> "Class"
+  | Else -> "Else"
+  | False -> "False"
+  | Fun -> "Fun"
+  | For -> "For"
+  | If -> "If"
+  | Nil -> "Nil"
+  | Or -> "Or"
+  | Print -> "Print"
+  | Return -> "Return"
+  | Super -> "Super"
+  | This -> "This"
+  | True -> "True"
+  | Var -> "Var"
+  | While -> "While"
+  | EndOfFile -> "EndOfFile"
+
+let string_of_wrapped token =
+  let of_raw = string_of_raw token.raw in
+  Printf.sprintf "%s; starts at line %i, col %i" of_raw token.start_line token.start_col
+
 let wrap_token raw_token start_line start_col : t =
   { raw = raw_token; start_line; start_col }
